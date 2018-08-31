3a13e1ed1affd652003f01293a9529d2a354edb2
BuildTools <unconfigured@null.spigotmc.org>
2018-08-31 21:50:55 +1000

    Implement TYPING_START event.

---
+++ src/main/java/me/xa5/discordjavalib/handler/WSHandlerTypingStart.java
@@ -0,0 +1,29 @@
+package me.xa5.discordjavalib.handler;
+
+import com.eclipsesource.json.JsonObject;
+import me.xa5.discordjavalib.WSClient;
+import me.xa5.discordjavalib.entities.DiscordApi;
+import me.xa5.discordjavalib.entities.Guild;
+import me.xa5.discordjavalib.entities.Member;
+import me.xa5.discordjavalib.entities.TextChannel;
+
+public class WSHandlerTypingStart extends WSEventHandler {
+    public WSHandlerTypingStart(DiscordApi api) {
+        super(api);
+    }
+
+    @Override
+    public String getInternalEventName() {
+        return "typing_start";
+    }
+
+    @Override
+    public void handle(WSClient client, JsonObject data) {
+        Guild guild = client.getApi().getGuild(data.get("guild_id").asString());
+        TextChannel channel = guild.getTextChannel(data.get("channel_id").asString());
+        Member member = guild.getMember(data.get("user_id").asString());
+
+        long timestamp = data.get("timestamp").asLong();
+        data.get("member").asObject();
+    }
+}
