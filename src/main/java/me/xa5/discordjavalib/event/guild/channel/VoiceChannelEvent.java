b46719d1b16dbf97c2624fd38469af7a0fa839ea
Blake Redenius <blakeredenius@gmail.com>
2018-09-02 00:45:26 +1000

    Actually assign channel parameter to field.

--- src/main/java/me/xa5/discordjavalib/event/guild/channel/VoiceChannelEvent.java
+++ src/main/java/me/xa5/discordjavalib/event/guild/channel/VoiceChannelEvent.java
@@ -1,14 +1,14 @@
 package me.xa5.discordjavalib.event.guild.channel;
 
-import me.xa5.discordjavalib.entities.Channel;
 import me.xa5.discordjavalib.entities.DiscordApi;
 import me.xa5.discordjavalib.entities.VoiceChannel;
 
 public class VoiceChannelEvent extends ChannelEvent {
     private VoiceChannel voiceChannel;
 
-    public VoiceChannelEvent(DiscordApi api, Channel channel) {
+    public VoiceChannelEvent(DiscordApi api, VoiceChannel channel) {
         super(api, channel);
+        this.voiceChannel = channel;
     }
 
     public VoiceChannel getVoiceChannel() {
